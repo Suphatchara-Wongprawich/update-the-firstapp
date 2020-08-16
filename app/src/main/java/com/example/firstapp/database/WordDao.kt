0f69cff3c0f53f0cbbd56528b3d5c758ffdaccab
Suphatchara-Wongprawich <68465404+Suphatchara-Wongprawich@users.noreply.github.com>
2020-08-16 22:53:13 +0700

    update-the-firstapp-Update WordDao

--- app/src/main/java/com/example/firstapp/database/WordDao.kt
+++ app/src/main/java/com/example/firstapp/database/WordDao.kt
@@ -1,15 +1,19 @@
 package com.example.firstapp.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
 @Dao
 interface WordDao {
 
-
     @Query("SELECT * from word_table ORDER BY word ASC")
-    fun getAlphabetizedWords(): LiveData<List<Word>>
+    fun getAlphabetizedWords(): List<Word>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(word: Word)
+    suspend fun insert(word: Word)
 
     @Query("DELETE FROM word_table")
-    fun deleteAll()
+    suspend fun deleteAll()
 }
