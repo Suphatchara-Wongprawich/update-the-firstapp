6d5475c93966076d407ef38f1dd489889ec1c2bf
Suphatchara-Wongprawich <68465404+Suphatchara-Wongprawich@users.noreply.github.com>
2020-08-16 22:57:39 +0700

    update-the-firstapp-Update Repository

--- app/src/main/java/com/example/firstapp/database/WordRepository.kt
+++ app/src/main/java/com/example/firstapp/database/WordRepository.kt
@@ -1,16 +1,13 @@
 package com.example.firstapp.database
 
-import android.arch.lifecycle.LiveData
-import android.support.annotation.WorkerThread
+
+import androidx.lifecycle.LiveData
 
 class WordRepository(private val wordDao: WordDao) {
 
 
     val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
 
-
-    @Suppress("RedundantSuspendModifier")
-    @WorkerThread
     suspend fun insert(word: Word) {
         wordDao.insert(word)
     }
