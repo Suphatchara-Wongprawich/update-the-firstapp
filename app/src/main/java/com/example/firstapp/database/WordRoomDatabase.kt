a6a70de9c7dbce473ecd10b543cf289ee8aa8156
Suphatchara-Wongprawich <68465404+Suphatchara-Wongprawich@users.noreply.github.com>
2020-08-16 22:56:16 +0700

    update-the-firstapp-Update Roomdatabase

--- app/src/main/java/com/example/firstapp/database/WordRoomDatabase.kt
+++ app/src/main/java/com/example/firstapp/database/WordRoomDatabase.kt
@@ -1,6 +1,13 @@
 package com.example.firstapp.database
 
 import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Database(entities = [Word::class], version = 1)
 abstract class WordRoomDatabase : RoomDatabase() {
