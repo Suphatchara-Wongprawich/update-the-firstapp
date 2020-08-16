94dac1174bb76c30f7508c411a9cfc64ec62dfd7
Suphatchara-Wongprawich <68465404+Suphatchara-Wongprawich@users.noreply.github.com>
2020-08-16 22:29:03 +0700

    update-the-firstapp-Add WordViewModel

--- app/src/main/java/com/example/firstapp/database/WordViewModel.kt
+++ app/src/main/java/com/example/firstapp/database/WordViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.firstapp.database
 
-class WordViewModel {
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
+
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: WordRepository
+
+    val allWords: LiveData<List<Word>>
+
+    init {
+        val wordsDao = WordRoomDatabase.getDatabase(application, viewModelScope).wordDao()
+        repository = WordRepository(wordsDao)
+        allWords = repository.allWords
+    }
+
+
+    fun insert(word: Word) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(word)
+    }
 }
